6e61ff7978b46563078e7225977e836bf5451a79
SuJung Eo <soojung.eo@buttonsoup.co>
2022-03-29 11:08:05 +0900

    fix autoUpdate

--- main.js
+++ main.js
@@ -31,30 +31,6 @@ app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
 })
 
-app.on('ready', function () {
-  require('update-electron-app')({
-    repo: 'sj-eo/ipc2',
-
-  });
+require('update-electron-app')({
+  repo: 'sj-eo/ipc2',
 });
-
-autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
-  const dialogOpts = {
-    type: 'info',
-    buttons: ['Restart', 'Later'],
-    title: 'Application Update',
-    message: process.platform === 'win32' ? releaseNotes : releaseName,
-    detail: 'A new version has been downloaded. Restart the application to apply the updates.'
-  }
-  dialog.showMessageBox(dialogOpts).then((returnValue) => {
-    if (returnValue.response === 0) {
-      autoUpdater.quitAndInstall()
-    }
-  })
-})
-
-autoUpdater.on('error', message => {
-  console.error('There was a problem updating the application')
-  console.error(message)
-})
-
